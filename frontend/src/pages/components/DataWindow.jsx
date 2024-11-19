8dace5390cbb688491cfbc7a21ee763cf1df7cbc
Jamie <jamiewong198@gmail.com>
2024-11-19 16:18:59 -0500

    added simple download button

--- frontend/src/pages/components/DataWindow.jsx
+++ frontend/src/pages/components/DataWindow.jsx
@@ -20,6 +20,17 @@ const DataWindow = ({ data }) => {
         )
     ) : [];
 
+    const handleDownload = () => {
+        const dataStr = JSON.stringify(data, null, 2); // Convert data to string with pretty-printing
+        const blob = new Blob([dataStr], { type: 'application/json' });
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement('a');
+        link.href = url;
+        link.download = 'data.json';
+        link.click();
+        URL.revokeObjectURL(url);
+      };
+
     return (
         <div>
             <input 
@@ -28,6 +39,10 @@ const DataWindow = ({ data }) => {
                 value={searchTerm}
                 onChange={handleSearchChange}
             />
+            <button className='download'
+                onClick={handleDownload}
+            >
+                download</button>
             {filteredData.length > 0 ? (
                 <ol>
                     {filteredData.map((item, index) => (
