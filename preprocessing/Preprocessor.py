430ef227ae1097e79bc02544c41c0017cab47d34
Brandon Carrasco <carrascb@mcmaster.ca>
2025-04-03 14:46:18 -0400

    Updated Preprocessors Default Parameters
    As per Daniel's recommendation; should fail less now.

--- preprocessing/Preprocessor.py
+++ preprocessing/Preprocessor.py
@@ -19,9 +19,9 @@ DEFAULT_PARAMS = {
         "half_window" : 4,    
         "log_transform" : np.cbrt, 
         "num_guesses" : 5,   
-        "num_iters" : 200,    
+        "num_iters" : 500,    
         "significance" : 0.05,  
-        "max_k" : 4, 
+        "max_k" : 6, 
         "k" : None,
         "segment_constrain" : True # Specifies if the segment types should be constrained to 0 (lingering episodes) and 1 (progression episodes). False = more movement types than just two.
     }
